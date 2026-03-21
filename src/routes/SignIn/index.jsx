66750bec770321a97e496277ffefa0930e72452d
jdnjsyoo <jdnjsyoo@snu.ac.kr>
2026-03-21 17:19:58 +0900

    first commit

--- src/routes/SignIn/index.jsx
+++ src/routes/SignIn/index.jsx
@@ -1,4 +1,12 @@
 //TODO: 로그인 페이지 구현
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
+
 export default function SignIn() {
   return <div>SignIn</div>;
 }
