52af28d16d3934ae8710167dc140765d72f6ce60
yanghyeonseo <yanghyeonseo1018@gmail.com>
2026-03-21 17:31:31 +0900

    fix: SignIn/index.jsx import

--- src/routes/SignIn/index.jsx
+++ src/routes/SignIn/index.jsx
@@ -1,3 +1,11 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
+
 //TODO: 로그인 페이지 구현
 export default function SignIn() {
   return <div>SignIn</div>;
