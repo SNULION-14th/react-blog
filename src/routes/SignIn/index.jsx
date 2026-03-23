d5a0eb20e0d57051c1402a2b24994ffefc83519f
yanghyeonseo <yanghyeonseo1018@gmail.com>
2026-03-23 21:56:31 +0900

    feat: signup page

--- src/routes/SignIn/index.jsx
+++ src/routes/SignIn/index.jsx
@@ -1,11 +1,4 @@
 import { Input, Button } from "@/shared/components";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardFooter,
-} from "@/components/ui/card";
 import logo from "@/assets/logo.png";
 
 //TODO: 로그인 페이지 구현
@@ -15,13 +8,10 @@ export default function SignIn() {
       <div className="flex flex-col justify-center items-center bg-card text-card-foreground rounded-xl border shadow-sm w-md">
         <img src={logo} className="w-40" />
         <p className="font-bold text-2xl">로그인</p>
-        <div
-          id="input-section"
-          className="flex flex-col gap-2 px-10 mt-10 mb-5 w-full"
-        >
+        <div className="flex flex-col gap-2 px-10 mt-10 mb-5 w-full">
           <Input
             className="focus-visible:ring-amber-500 focus-visible:ring-2 focus-visible:border-transparent selection:bg-amber-300 selection:text-black"
-            type="id"
+            type="text"
             placeholder="아이디를 입력하세요"
           />
           <Input
