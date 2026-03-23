c40a8486cc51ccf468f03ef29f33d76a18ec5387
Suhyeon An <sh-an@naver.com>
2026-03-23 15:17:59 +0900

    add basic components to sign-in page

--- src/routes/SignIn/index.jsx
+++ src/routes/SignIn/index.jsx
@@ -1,4 +1,4 @@
-// ⬇️추가
+import logo from "@/assets/logo.png";
 import {
   Card,
   CardContent,
@@ -6,12 +6,32 @@ import {
   CardTitle,
   CardFooter,
 } from "@/components/ui/card";
-
-export default function SignIn() {
-  return <div>SignIn</div>;
-}
+import { Button, Input } from "@/shared/components";
 
 //TODO: 로그인 페이지 구현
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <div>
+        <Card className="card signin-container">
+          <CardHeader className="card signin-header">
+            <img src={logo} alt="logo" />
+            <CardTitle>로그인</CardTitle>
+          </CardHeader>
+          <CardContent className="card signin-content">
+            <Input type="text" placeholder="아이디를 입력하세요"></Input>
+            <Input type="password" placeholder="비밀번호를 입력하세요"></Input>
+          </CardContent>
+          <CardFooter className="card signin-footer">
+            <Button variant="default" size="default">
+              로그인
+            </Button>
+            <Button variant="default" size="default">
+              회원 가입
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </>
+  );
 }
