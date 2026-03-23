81a95d8930124f28ccfd41fb9f089c1df32f1da0
yanghyeonseo <yanghyeonseo1018@gmail.com>
2026-03-23 20:47:25 +0900

    feat: home page

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,72 +1,94 @@
-import * as React from "react"
+import * as React from "react";
+import { TagList } from "./tag";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Card({
-  className,
-  ...props
-}) {
+function Card({ className, post, ...props }) {
   return (
-    (<div
+    <div
       data-slot="card"
-      className={cn("bg-card text-card-foreground rounded-xl border shadow-sm", className)}
-      {...props} />)
+      className={cn(
+        "bg-card text-card-foreground rounded-xl border shadow-sm",
+        className,
+      )}
+      {...props}
+    >
+      <CardHeader>
+        <CardTitle title={post.title}></CardTitle>
+        <CardDescription author={post.author.username}></CardDescription>
+      </CardHeader>
+      <CardContent tags={post.tags}></CardContent>
+      <CardFooter like_count={post.like_users.length}></CardFooter>
+    </div>
   );
 }
 
-function CardHeader({
-  className,
-  ...props
-}) {
+function CardHeader({ className, ...props }) {
   return (
-    (<div
+    <div
       data-slot="card-header"
-      className={cn("flex flex-col gap-1.5 p-6", className)}
-      {...props} />)
+      className={cn("flex flex-col gap-1.5 p-6 text-left", className)}
+      {...props}
+    />
   );
 }
 
-function CardTitle({
-  className,
-  ...props
-}) {
+function CardTitle({ className, title, ...props }) {
   return (
-    (<div
+    <div
       data-slot="card-title"
-      className={cn("leading-none font-semibold tracking-tight", className)}
-      {...props} />)
+      className={cn(
+        "leading-none font-semibold tracking-tight text-xl font-bold",
+        className,
+      )}
+      {...props}
+    >
+      {title}
+    </div>
   );
 }
 
-function CardDescription({
-  className,
-  ...props
-}) {
+function CardDescription({ className, author, ...props }) {
   return (
-    (<div
+    <div
       data-slot="card-description"
       className={cn("text-muted-foreground text-sm", className)}
-      {...props} />)
+      {...props}
+    >
+      {author}
+    </div>
   );
 }
 
-function CardContent({
-  className,
-  ...props
-}) {
-  return (<div data-slot="card-content" className={cn("p-6 pt-0", className)} {...props} />);
+function CardContent({ className, tags, ...props }) {
+  return (
+    <div
+      data-slot="card-content"
+      className={cn("p-6 pt-0", className)}
+      {...props}
+    >
+      <TagList contents={tags}></TagList>
+    </div>
+  );
 }
 
-function CardFooter({
-  className,
-  ...props
-}) {
+function CardFooter({ className, like_count, ...props }) {
   return (
-    (<div
+    <div
       data-slot="card-footer"
       className={cn("flex items-center p-6 pt-0", className)}
-      {...props} />)
+      {...props}
+    >
+      ❤️ {like_count}
+    </div>
   );
 }
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+};
