e4b281e5fd17e35ca69bfe0701ccd68c7b2f74e2
yerim <yerimryu@snu.ac.kr>
2026-03-23 22:44:37 +0900

    final

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -17,12 +17,51 @@ export default function Home() {
               placeholder="태그를 검색하세요"
             />
           </div>
-        </div>
+          <div className="flex flex-wrap justify-center gap-2 mb-12 max-w-3xl px-4">
+            {(() => {
+              const allTagContents = posts.flatMap((post) =>
+                post.tags.map((t) => t.content),
+              );
+              const uniqueTags = Array.from(new Set(allTagContents));
 
-        <div className="mx-auto grid grid-cols-1 gap-y-4 md:grid-cols-2 lg:grid-cols-3 px-10 mt-10 lg:w-[950px] md:w-[640px] w-[320px]">
-          {/* TODO: 검색 결과 포스트 만들기 */}
+              return uniqueTags.map((tagContent, index) => (
+                <div
+                  key={index}
+                  className="mt-8 bg-amber-500 text-white px-4 py-1 rounded-md text-xs font-bold shadow-sm h-auto cursor-pointer"
+                >
+                  #{tagContent}
+                </div>
+              ));
+            })()}
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-4xl mt-1 px-4 mx-auto">
+            {posts.map((post) => (
+              <div className="flex flex-col h-full p-5 border border-gray-300 rounded-xl shadow-sm hover:shadow-md transition-all bg-white text-left">
+                <div className="text-2xl font-bold">{post.title}</div>
+                <p className="text-sm text-gray-500 mt-1">
+                  {post.author.username}
+                </p>
+                <div className="flex flex-row justify-start flex-wrap gap-2 w-full mt-4">
+                  {post.tags.map((tag) => (
+                    <div
+                      key={tag.id}
+                      className="mt-8 bg-amber-500 text-white px-4 py-1 rounded-md text-xs font-bold shadow-sm h-auto cursor-pointer"
+                    >
+                      {tag.content}
+                    </div>
+                  ))}
+                </div>
+                <div className="flex flex-row justify-start mt-6 items-center text-black font-bold">
+                  <span className="mr-1">❤️</span>
+                  {post.like_users.length}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
+
+      <div className="mx-auto grid grid-cols-1 gap-y-4 md:grid-cols-2 lg:grid-cols-3 px-10 mt-10 lg:w-[950px] md:w-[640px] w-[320px]"></div>
     </>
   );
 }
--- src/routes/SignIn/index.jsx
+++ src/routes/SignIn/index.jsx
@@ -1,4 +1,51 @@
-//TODO: 로그인 페이지 구현
-export default function SignIn() {
-  return <div>SignIn</div>;
-}
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
+import logoImg from "@/assets/logo.png";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const SignIn = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Card className="w-[400px]">
+        <CardHeader>
+          <img src={logoImg} alt="logo" className="w-20 mx-auto" />
+          <CardTitle className="text=center text-2xl">로그인</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Input
+            type="text"
+            placeholder="아이디를 입력하세요"
+            className="mb-4 focus-visible:ring-amber-500 focus-visible:border-amber-500"
+          />
+          <Input
+            type="password"
+            placeholder="비밀번호를 입력하세요"
+            className="mb-4 focus-visible:ring-amber-500 focus-visible:border-amber-500"
+          />
+        </CardContent>
+        <CardFooter className="flex flex-row justify-center -mt-6">
+          <Button
+            variant="outline"
+            className="w-20 hover:!bg-amber-500 hover:!text-white hover:!border-amber-500 transition-colors"
+          >
+            로그인
+          </Button>
+          <Button
+            variant="outline"
+            className="w-20 hover:!bg-amber-500 hover:!text-white hover:!border-amber-500 transition-colors"
+          >
+            회원가입
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default SignIn;
