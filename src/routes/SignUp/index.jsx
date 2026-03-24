310acd596eb9840db091159ceabf49fc3943b624
Suhyeon An <sh-an@naver.com>
2026-03-24 20:54:23 +0900

    signup page

--- src/routes/SignUp/index.jsx
+++ src/routes/SignUp/index.jsx
@@ -8,60 +8,59 @@ import {
 } from "@/components/ui/card";
 import { Button, Input } from "@/shared/components";
 
-//TODO: 회원가입 페이지 구현
 export default function SignUp() {
   return (
     <>
-      <div>
-        <Card>
+      <div className="w-full min-h-screen flex items-center justify-center">
+        <Card className="w-sm flex flex-col items-center justify-center">
           <CardHeader>
-            <img src={logo} alt="logo"></img>
-            <CardTitle>회원가입</CardTitle>
+            <img className="w-30 h-30" src={logo} alt="logo" />
+            <CardTitle className="text-xl">회원가입</CardTitle>
           </CardHeader>
-          <CardContent>
-            <div>
-              <label>아이디</label>
+          <CardContent className="flex flex-col items-center justify-center w-full pb-1 m-2 gap-2 text-left">
+            <div className="w-full mb-0.5">
+              <div className="text-sm font-semibold mb-1">아이디</div>
               <Input type="text" placeholder="아이디를 입력하세요"></Input>
             </div>
 
-            <div>
-              <label>비밀번호</label>
+            <div className="w-full mb-0.5">
+              <div className="text-sm font-semibold mb-1">비밀번호</div>
               <Input
                 type="password"
                 placeholder="비밀번호를 입력하세요"
               ></Input>
             </div>
 
-            <div>
-              <label>비밀번호 확인</label>
+            <div className="w-full mb-0.5">
+              <div className="text-sm font-semibold mb-1">비밀번호 확인</div>
               <Input
                 type="password"
                 placeholder="비밀번호를 입력하세요"
               ></Input>
             </div>
 
-            <div>
-              <label>이름</label>
+            <div className="w-full mb-0.5">
+              <label className="text-sm font-semibold mb-1">이름</label>
               <Input type="text" placeholder="이름을 입력하세요"></Input>
             </div>
 
-            <div>
-              <label>이메일</label>
+            <div className="w-full mb-0.5">
+              <label className="text-sm font-semibold mb-1">이메일</label>
               <Input type="text" placeholder="이메일을 입력하세요"></Input>
             </div>
 
-            <div>
-              <label>학교</label>
+            <div className="w-full mb-0.5">
+              <label className="text-sm font-semibold mb-1">학교</label>
               <Input type="text" placeholder="학교를 입력하세요"></Input>
             </div>
 
-            <div>
-              <label>전공</label>
+            <div className="w-full mb-0.5">
+              <label className="text-sm font-semibold mb-1">전공</label>
               <Input type="text" placeholder="전공을 입력하세요"></Input>
             </div>
           </CardContent>
-          <CardFooter>
-            <Button>회원가입</Button>
+          <CardFooter className="flex flex-col items-center justify-center w-full pb-1 m-2 gap-2">
+            <Button className="w-full">회원가입</Button>
           </CardFooter>
         </Card>
       </div>
