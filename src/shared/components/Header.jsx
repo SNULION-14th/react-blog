fa7fd45eb208771c12111c83be20a92611955fca
yanghyeonseo <yanghyeonseo1018@gmail.com>
2026-03-30 21:21:14 +0900

    feat: Header when logged in

--- src/shared/components/Header.jsx
+++ src/shared/components/Header.jsx
@@ -2,10 +2,12 @@ import lion from "@/assets/lion.jpeg";
 import { useMediaQuery } from "@/shared/hooks";
 import { Button } from "@/shared/components";
 import { Link } from "react-router-dom";
+import { useLogIn } from "@/contexts/LogInContext";
 
 //TODO: 로그인 상태에 따라 버튼을 바꾸고, 로그아웃 기능 추가
 export const Header = () => {
   const isMobile = useMediaQuery("(max-width: 640px)");
+  const { isLoggedIn, logOut } = useLogIn();
   // 로그인 상태에 따라 버튼을 바꾸고, 로그아웃 기능 추가
 
   return (
@@ -18,7 +20,7 @@ export const Header = () => {
           <div className="text-xl">SNULION BLOG</div>
         </div>
       </Link>
-      {isMobile ? null : (
+      {isMobile ? null : !isLoggedIn ? (
         <div className="flex flex-row gap-5">
           <Link to="/signin">
             <Button>sign in</Button>
@@ -27,6 +29,8 @@ export const Header = () => {
             <Button>sign up</Button>
           </Link>
         </div>
+      ) : (
+        <Button onClick={logOut}>로그아웃</Button>
       )}
     </div>
   );
