1072d8c617261210b92e4a2ee2e1c64b50129ea3
gomja <kategom@snu.ac.kr>
2026-03-30 18:49:21 +0900

    refactor

--- src/shared/components/Header.jsx
+++ src/shared/components/Header.jsx
@@ -2,11 +2,17 @@ import lion from "@/assets/lion.jpeg";
 import { useMediaQuery } from "@/shared/hooks";
 import { Button } from "@/shared/components";
 import { Link } from "react-router-dom";
+import { useUser } from "@/shared/context/userContext";
 
 //TODO: 로그인 상태에 따라 버튼을 바꾸고, 로그아웃 기능 추가
 export const Header = () => {
   const isMobile = useMediaQuery("(max-width: 640px)");
   // 로그인 상태에 따라 버튼을 바꾸고, 로그아웃 기능 추가
+  const { user, toggleUser } = useUser();
+
+  const handleLogout = () => {
+    toggleUser(null); // 로그아웃 시 사용자 정보를 null로 설정
+  };
 
   return (
     <div
@@ -20,12 +26,21 @@ export const Header = () => {
       </Link>
       {isMobile ? null : (
         <div className="flex flex-row gap-5">
-          <Link to="/signin">
-            <Button>sign in</Button>
-          </Link>
-          <Link to="/signup">
-            <Button>sign up</Button>
-          </Link>
+          {user ? (
+            <>
+              <span>{user.name}님</span>
+              <Button onClick={handleLogout}>logout</Button>
+            </>
+          ) : (
+            <>
+              <Link to="/signin">
+                <Button>sign in</Button>
+              </Link>
+              <Link to="/signup">
+                <Button>sign up</Button>
+              </Link>
+            </>
+          )}
         </div>
       )}
     </div>
--- src/shared/context/userContext.jsx
+++ src/shared/context/userContext.jsx
@@ -1,1 +1,18 @@
 //TODO: 사용자 로그인 상태 관리 구현
+import { createContext, useState, useContext } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const toggleUser = (userInfo) => {
+    setUser(userInfo);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, toggleUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
