107e134a1c695895b2e19d302634a9742d3c7ff5
Suhyeon An <sh-an@naver.com>
2026-03-31 20:35:34 +0900

    feat: UserContext & UserProvider

--- src/shared/context/userContext.jsx
+++ src/shared/context/userContext.jsx
@@ -1,1 +1,39 @@
-//TODO: 사용자 로그인 상태 관리 구현
+import { createContext, useState, useContext } from "react";
+
+const UserContext = createContext(null);
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    username: "",
+    isLoggedIn: false,
+  });
+
+  const setLogIn = (username) => {
+    setUser((prev) => ({ ...prev, username: username, isLoggedIn: true }));
+  };
+
+  const setLogOut = () => {
+    setUser((prev) => ({ ...prev, username: null, isLoggedIn: false }));
+  };
+
+  const value = {
+    username: user.username,
+    isLoggedIn: user.isLoggedIn,
+    setLogIn: setLogIn,
+    setLogOut: setLogOut,
+  };
+
+  return (
+    <>
+      <UserContext.Provider value={value}>{children}</UserContext.Provider>
+    </>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+};
