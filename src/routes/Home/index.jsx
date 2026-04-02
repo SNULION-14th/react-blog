70bac5de5002d23195836a6561afc1a39bc306ff
Suhyeon An <sh-an@naver.com>
2026-04-02 10:53:00 +0900

    feat: add tag search

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -8,37 +8,77 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
-  const [searchTags, setSearchTags] = useState([]);
+  const [tagInput, setTagInput] = useState("");
   const [storedTags, setStoredTags] = useState([]);
 
   const { isLoggedIn } = useUser();
   const navigate = useNavigate();
 
+  const tagsToShow =
+    tagInput === ""
+      ? storedTags
+      : storedTags.filter((t) => t.content.includes(tagInput));
+
+  const postsToShow =
+    tagInput === ""
+      ? posts
+      : posts.filter((post) =>
+          post.tags.some((t) => t.content.includes(tagInput.trim())),
+        );
+
   const fetchPosts = async () => {
     const posts = await getPosts();
-    console.log("post fetch response", posts);
+    // console.log("post fetch response", posts);
+    return posts;
   };
 
   const fetchTags = async () => {
     const tags = await getTags();
-    console.log("tag fetch response", tags);
+    // console.log("tag fetch response", tags);
+    return tags;
   };
 
   const handleSearchTagInputChange = (e) => {
     const { value } = e.target;
-    console.log("search tag input change", value);
+    setTagInput(value);
+    // console.log("search tag input change", value);
   };
 
   const handleCreatePost = async (post, author) => {
-    const createResponse = await createPost({
+    const formattedPost = {
       ...post,
+      tags: post.tags.map((t) => t.content),
+    };
+
+    const createResponse = await createPost({
+      ...formattedPost,
       author,
     });
+
     const newPost = await getPostById(createResponse.postId);
-    console.log("new post", newPost);
+    setPosts((prev) => [...prev, newPost]);
+    // console.log("new post", newPost);
+    return newPost;
   };
 
-  // TODO: 로그인한 사용자 정보를 가져와서 PostDialog에 전달하고, 게시글 작성 버튼 추가
+  // load all posts
+  useEffect(() => {
+    const initPosts = async () => {
+      setPosts(await fetchPosts());
+    };
+
+    initPosts();
+  }, []);
+
+  // load all stored tags
+  useEffect(() => {
+    const initTags = async () => {
+      setStoredTags(await fetchTags());
+    };
+
+    initTags();
+  }, []);
+
   return (
     <div className="pb-20 pt-14">
       <div className="flex flex-col justify-center items-center mb-5">
@@ -46,36 +86,44 @@ export default function Home() {
           <h1 className="uppercase text-6xl text-black">my blog</h1>
         </div>
         <div className="w-[90vw] max-w-md flex justify-center">
-          <Input type="text" placeholder="태그로 검색하기" />
+          <Input
+            type="text"
+            placeholder="태그로 검색하기"
+            onChange={handleSearchTagInputChange}
+          />
         </div>
         <div className="flex mt-5 justify-center flex-wrap">
-          {searchTags.map((tag) => {
-            return <TagBadge key={tag.id} tag={tag} />;
-          })}
+          {tagsToShow.map((tag) => (
+            <TagBadge key={tag.id} tag={tag}></TagBadge>
+          ))}
         </div>
       </div>
 
       <div className="mx-auto grid grid-cols-1 gap-y-4 md:grid-cols-2 lg:grid-cols-3 px-10 mt-10 lg:w-[950px] md:w-[640px] w-[320px]">
-        {posts.map((post) => (
+        {postsToShow.map((post) => (
           <div
             key={post.id}
             className="w-full flex justify-center items-center"
           >
-            <SmallPost
-              post={post}
-              onClick={() => {
-                console.log(post.id);
-                navigate(`/post/${post.id}`);
-              }}
-            />
+            {
+              <SmallPost
+                post={post}
+                onClick={() => {
+                  console.log(post.id);
+                  navigate(`/post/${post.id}`);
+                }}
+              />
+            }
           </div>
         ))}
       </div>
-      {isLoggedIn ? (
-        <PostDialog onCreate={handleCreatePost}></PostDialog>
-      ) : (
-        <></>
-      )}
+      <div className="m-5 p-5">
+        {isLoggedIn ? (
+          <PostDialog onCreate={handleCreatePost}></PostDialog>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
